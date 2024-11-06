93ce24a4a0703a7b8227d901f217d22e5a11cedb
Mindia Arabuli <65043119+Mind1a@users.noreply.github.com>
2024-11-06 21:30:36 +0400

    update

---
+++ app/(logged-in)/my-account/two-factor-auth-form/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+type Props = {
+  twoFactorActivated: any;
+};
+
+export default function TwoFactorAuthForm({ twoFactorActivated }: Props) {
+  return <div>TwoFactorAuthForm</div>;
+}
